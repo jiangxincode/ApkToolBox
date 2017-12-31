e480d5a34470db618136e6776c946c98dbcdfee6
jiangxincode <jiangxinnju@163.com>
2017-12-31 12:20:51 +0800

    update

---
+++ ApkToolBox.ps1
@@ -0,0 +1,17 @@
+﻿$CURRENT_DIR = Split-Path -Parent $MyInvocation.MyCommand.Definition
+echo "CURRENT_DIR is: $CURRENT_DIR"
+echo "===============================================delete tmp start"
+Remove-Item "$CURRENT_DIR/tmp" -recurse
+echo "===============================================delete tmp end"
+echo "===============================================apktool start"
+java -jar "-Duser.language=en" "-Dfile.encoding=UTF8" "$CURRENT_DIR/apktool.jar" d -f "$CURRENT_DIR/test.apk" -o "$CURRENT_DIR/tmp/apktool/"
+echo "===============================================apktool end"
+echo "===============================================7zip start"
+7z.exe x "test.apk" -o"$CURRENT_DIR/tmp/7zip/" -y
+echo "===============================================7zip end"
+echo "===============================================dex2jar start"
+dex2jar-2.1-SNAPSHOT/d2j-dex2jar.bat "$CURRENT_DIR/tmp/7zip/classes.dex" -o "$CURRENT_DIR/tmp/dex2jar/classes.jar"
+echo "===============================================dex2jar end"
+echo "===============================================jd-gui start"
+jd-gui.exe "$CURRENT_DIR/tmp/dex2jar/classes.jar"
+echo "===============================================jd-gui end"
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## Apktool
 
-<https://ibotpeaches.github.io/Apktool/>
+* <https://ibotpeaches.github.io/Apktool/>
 
 * 反编译获取资源文件: `.\apktool.bat d -f .\test.apk -o test`
 * 编译获取APK: `.\apktool.bat b .\test\`
@@ -17,3 +17,31 @@
 ## jd-gui
 
 * <http://jd.benow.ca/>
+
+## AXMLPrinter3
+
+`java -jar AXMLPrinter3.jar .\AndroidManifest.xml > .\AndroidManifest_1.xml`
+
+## APKSign
+
+有了这个软件，可以自己修改美化APK文件。用WINRAR打开APK文件，将自己的图片图标放进替换原有的，然后用本软件制作签名，然后安装进Android.或者下载别人制作好的ROM，自己精简删除，或者添加APK文件，用本软件给ROM制作数字签名，然后刷机。
+
+`java -jar signapk.jar testkey.x509.pem testkey.pk8　test.apk test1.apk`
+
+## smali/baksmali
+
+* <https://github.com/JesusFreke/smali>
+
+`java -jar .\baksmali-2.1.3.jar -o classout/ classes.dex`
+
+`java -jar smali-2.1.3.jar classout/ -o classes1.dex`
+
+## dedexer
+
+* https://sourceforge.net/projects/dedexer/
+
+`java -jar .\ddx1.26.jar -o -D -d test classes.dex`
+
+## dexdump
+
+`dexdump.exe -d classes.dex > spk.txt`
