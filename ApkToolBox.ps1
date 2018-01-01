946f4d6f3d7bcb971a769d523e9e896b81456c88
jiangxincode <jiangxinnju@163.com>
2018-01-01 14:28:29 +0800

    update

--- ApkToolBox.ps1
+++ ApkToolBox.ps1
@@ -17,7 +17,7 @@ function APKToolDecode ($IN_PUT, $OUT_PUT) {
 }
 
 function APKToolEncode ($IN_PUT, $OUT_PUT) {
-    Write-Output "===============================================APKToolDecode start"
+    Write-Output "===============================================APKToolEncode start"
 
     Write-Output "INPUT is: $IN_PUT, and OUTPUT is: $OUT_PUT"
 
@@ -28,7 +28,7 @@ function APKToolEncode ($IN_PUT, $OUT_PUT) {
     Write-Output "===============================================delete $OUT_PUT end"
     
     java -jar "-Duser.language=en" "-Dfile.encoding=UTF8" "$CURRENT_DIR/apktool.jar" b $IN_PUT -o $OUT_PUT
-    Write-Output "===============================================APKToolDecode end"
+    Write-Output "===============================================APKToolEncode end"
 }
 
 function 7Zip ($IN_PUT, $OUT_PUT) {
@@ -42,7 +42,7 @@ function 7Zip ($IN_PUT, $OUT_PUT) {
     }
     Write-Output "===============================================delete $OUT_PUT end"
     
-    7z.exe x $IN_PUT -o"$OUT_PUT" -y
+    ./7z.exe x $IN_PUT -o"$OUT_PUT" -y
     Write-Output "===============================================7Zip end"
 }
 
@@ -63,7 +63,7 @@ function ZipAlign ($IN_PUT, $OUT_PUT) {
     If (!(Test-Path $OUT_PUT)) {
         New-Item -ItemType Directory -Force -Path $OUT_PUT
     }
-    zipalign.exe -f -v 4 $IN_PUT "$OUT_PUT/destination.apk"
+    ./zipalign.exe -f -v 4 $IN_PUT "$OUT_PUT/destination.apk"
     Write-Output "===============================================zipalign end"
 }
 
@@ -85,22 +85,93 @@ function JDGUI ($IN_PUT) {
     Write-Output "===============================================jd-gui end"
 }
 
+function DexDump ($IN_PUT, $OUT_PUT) {
+    Write-Output "===============================================DexDump start"
+
+    if (Test-Path $OUT_PUT) {
+        Remove-Item $OUT_PUT -recurse
+    }
+    If (!(Test-Path $OUT_PUT)) {
+        New-Item -ItemType Directory -Force -Path $OUT_PUT
+    }
+    ./dexdump.exe -d $IN_PUT > "$OUT_PUT/dexdump.dump"
+    Write-Output "===============================================DexDump end"
+}
+
+function DeDexer ($IN_PUT, $OUT_PUT) {
+    Write-Output "===============================================DeDexer start"
+
+    if (Test-Path $OUT_PUT) {
+        Remove-Item $OUT_PUT -recurse
+    }
+    If (!(Test-Path $OUT_PUT)) {
+        New-Item -ItemType Directory -Force -Path $OUT_PUT
+    }
+    java -jar "$CURRENT_DIR/ddx1.26.jar" -o -D -d $OUT_PUT $IN_PUT
+    Write-Output "===============================================DeDexer end"
+}
+
+function SignApk ($IN_PUT, $OUT_PUT) {
+    Write-Output "===============================================SignApk start"
+
+    if (Test-Path $OUT_PUT) {
+        Remove-Item $OUT_PUT -recurse
+    }
+    If (!(Test-Path $OUT_PUT)) {
+        New-Item -ItemType Directory -Force -Path $OUT_PUT
+    }
+    java -jar "$CURRENT_DIR/APKSign/signapk.jar" "$CURRENT_DIR/APKSign/testkey.x509.pem" "$CURRENT_DIR/APKSign/testkey.pk8"　$IN_PUT "$OUT_PUT/signed.apk"
+    Write-Output "===============================================SignApk end"
+}
+
+function baksmali ($IN_PUT, $OUT_PUT) {
+    Write-Output "===============================================SignApk start"
+
+    if (Test-Path $OUT_PUT) {
+        Remove-Item $OUT_PUT -recurse
+    }
+    If (!(Test-Path $OUT_PUT)) {
+        New-Item -ItemType Directory -Force -Path $OUT_PUT
+    }
+    java -jar "$CURRENT_DIR/baksmali-2.1.3.jar" -o "$OUT_PUT" $IN_PUT
+    Write-Output "===============================================SignApk end"
+}
+
+function smali ($IN_PUT, $OUT_PUT) {
+    Write-Output "===============================================SignApk start"
+
+    if (Test-Path $OUT_PUT) {
+        Remove-Item $OUT_PUT -recurse
+    }
+    If (!(Test-Path $OUT_PUT)) {
+        New-Item -ItemType Directory -Force -Path $OUT_PUT
+    }
+    java -jar "$CURRENT_DIR/smali-2.1.3.jar" $IN_PUT -o "$OUT_PUT/classes_new.dex"
+    Write-Output "===============================================SignApk end"
+}
+
 
 
 if ($help) {
     Write-Output "Usage: `t .\ApkToolBox.ps1 -in input -out output -option [0, 1, 2, ...]"
-    Write-Output "Comment: `t [input] is a file and [output] is a directory"
+    Write-Output "Comment: `t [input] is a file/directory and [output] is a directory"
     Write-Output "Author: `t Aloys, jiangxinnju@163.com"
     Write-Output "Update From: `t https://github.com/jiangxincode/APKDecompiler"
     Write-Output "Option List:"
-    Write-Output "`t 0: common. [input] is a file and [output] is a directory"
-    Write-Output "`t 10: decompile apk using apktool. [input] is a file and [output] is a directory"
-    Write-Output "`t 11: build apk using apktool. [input] is a directory and [output] is a file"
-    Write-Output "`t 20: decompression apk using 7zip. [input] is a file and [output] is a directory"
-    Write-Output "`t 30: convert dex to jar using dex2jar. [input] is a file and [output] is a directory"
-    Write-Output "`t 40: open jar using jd-gui. [input] is a file and [output] is a directory"
-    Write-Output "`t 50: align apk using zipalign. [input] is a file and [output] is a directory"
-    Write-Output "`t 60: decode AndroidManifest.xml using AXMLPrinter3. [input] is a file and [output] is a directory"
+    Write-Output "`t 0: common. [input] is a file"
+    Write-Output "`t 10: decompile apk using apktool. [input] is a file"
+    Write-Output "`t 11: build apk using apktool. [input] is a directory"
+    Write-Output "`t 20: decompression apk using 7zip. [input] is a file"
+    Write-Output "`t 30: convert dex to jar using dex2jar. [input] is a file"
+    Write-Output "`t 40: open jar using jd-gui. [input] is a file"
+    Write-Output "`t 50: align apk using zipalign. [input] is a file"
+    Write-Output "`t 60: decode AndroidManifest.xml using AXMLPrinter3. [input] is a file"
+    Write-Output "`t 70: get dump file using dexdump. [input] is a file"
+    Write-Output "`t 80: reate Jasmin-like source files from DEX files using dedexer. [input] is a file"
+    Write-Output "`t 90: sign the apk using signapk. [input] is a file"
+    Write-Output "`t 100: decompile the dex using baksmali. [input] is a file"
+    Write-Output "`t 110: compile the dex using smali. [input] is a directory"
+    
     return
 }
 
@@ -149,11 +220,11 @@ if ($option -eq 0) {
     return
 }
 elseif ($option -eq 10) {
-    APKToolDecode $IN_PUT "$OUT_PUT/apktool"
+    APKToolDecode $IN_PUT "$OUT_PUT/apktool_d"
     return
 }
 elseif ($option -eq 11) {
-    APKToolEncode $IN_PUT "$OUT_PUT"
+    APKToolEncode $IN_PUT "$OUT_PUT/apktool_e/new_apk.apk"
     return
 }
 elseif ($option -eq 20) {
@@ -178,6 +249,29 @@ elseif ($option -eq 50) {
 elseif ($option -eq 60) {
     7Zip $IN_PUT "$OUT_PUT/7zip"
     AXMLPrinter3 "$OUT_PUT/7zip/AndroidManifest.xml" "$OUT_PUT/AXMLPrinter3"
+    return
+}
+elseif ($option -eq 70) {
+    7Zip $IN_PUT "$OUT_PUT/7zip"
+    DexDump "$OUT_PUT/7zip/classes.dex" "$OUT_PUT/dexdump"
+    return
+}
+elseif ($option -eq 80) {
+    7Zip $IN_PUT "$OUT_PUT/7zip"
+    DeDexer "$OUT_PUT/7zip/classes.dex" "$OUT_PUT/dedexer"
+    return
+}
+elseif ($option -eq 90) {
+    SignApk $IN_PUT "$OUT_PUT/signapk"
+    return
+}
+elseif ($option -eq 100) {
+    7Zip $IN_PUT "$OUT_PUT/7zip"
+    baksmali "$OUT_PUT/7zip/classes.dex" "$OUT_PUT/baksmali"
+    return
+}
+elseif ($option -eq 110) {
+    smali $IN_PUT "$OUT_PUT/smali"
     return
 }
 else {
--- README.md
+++ README.md
@@ -4,30 +4,4 @@
 * 本项目依赖apktool、Dex2Jar、jd-gui、AXMLPrinter3等工具，版权归原工具作者所有。
 * 本项目欢迎大家共同修改。
 
-
-可以参考<http://blog.csdn.net/vipzjyno1/article/details/21039349/>，但是文中的工具版本过低，需要升级，且某些命令有所变更。如果出现Dex2Jar报错版本不支持，可以参考<http://blog.csdn.net/jltxgcy/article/details/52599353>重新编译源码。
-
-
-## APKSign
-
-有了这个软件，可以自己修改美化APK文件。用WINRAR打开APK文件，将自己的图片图标放进替换原有的，然后用本软件制作签名，然后安装进Android.或者下载别人制作好的ROM，自己精简删除，或者添加APK文件，用本软件给ROM制作数字签名，然后刷机。
-
-`java -jar signapk.jar testkey.x509.pem testkey.pk8　test.apk test1.apk`
-
-## smali/baksmali
-
-* <https://github.com/JesusFreke/smali>
-
-`java -jar .\baksmali-2.1.3.jar -o classout/ classes.dex`
-
-`java -jar smali-2.1.3.jar classout/ -o classes1.dex`
-
-## dedexer
-
-* https://sourceforge.net/projects/dedexer/
-
-`java -jar .\ddx1.26.jar -o -D -d test classes.dex`
-
-## dexdump
-
-`dexdump.exe -d classes.dex > spk.txt`
+如果出现Dex2Jar报错版本不支持，可以参考<http://blog.csdn.net/jltxgcy/article/details/52599353>重新编译源码。
